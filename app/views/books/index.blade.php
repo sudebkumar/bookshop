ff3b85e4c784cf5d814f805a8686f505f5e861e5
sudebkumar <sudeb.phy137@gmail.com>
2014-08-25 13:40:36 +0600

    modify index.blade.php todoapps commit

--- app/views/books/index.blade.php
+++ app/views/books/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/book')
 @section('main')
-    <h1>Welcome to my bookshop</h1>
+<h1>Welcome to my bookshop</h1>
 <p>{{ link_to_route('books.create', 'Create new book') }}</p>
 @if ($booksList->count())
 <table class="table table-striped table-bordered">
@@ -16,27 +16,27 @@
     </thead>
 
     <tbody>
-@foreach ($booksList as $book)
+    @foreach ($booksList as $book)
     <tr>
-      <td>{{ $book->id }}</td>
-      <td>{{ $book->isbn }}</td>
-      <td>{{ $book->title }}</td>
-      <td>{{ $book->author }}</td>
-      <td>{{ $book->publisher }}</td>
-      <td>@if ($book->language==1){{'English'}} @else{{'Spanish'}}
-                @endif</td>
-      <td>{{ link_to_route('books.show', 'Read', array($book->id),
-    array('class' => 'btn btn-primary')) }}</td>
-      <td>{{ link_to_route('books.edit', 'Update', array($book->id),
-    array('class' => 'btn btn-warning')) }}</td>
-      <td>
-        {{ Form::open(array('method'=> 'DELETE', 'route' =>
+        <td>{{ $book->id }}</td>
+        <td>{{ $book->isbn }}</td>
+        <td>{{ $book->title }}</td>
+        <td>{{ $book->author }}</td>
+        <td>{{ $book->publisher }}</td>
+        <td>@if ($book->language==1){{'English'}} @else{{'Spanish'}}
+            @endif</td>
+        <td>{{ link_to_route('books.show', 'Read', array($book->id),
+            array('class' => 'btn btn-primary')) }}</td>
+        <td>{{ link_to_route('books.edit', 'Update', array($book->id),
+            array('class' => 'btn btn-warning')) }}</td>
+        <td>
+            {{ Form::open(array('method'=> 'DELETE', 'route' =>
             array('books.destroy', $book->id))) }}
-        {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
-        {{ Form::close() }}
-      </td>
+            {{ Form::submit('Delete', array('class' => 'btn btn-danger')) }}
+            {{ Form::close() }}
+        </td>
     </tr>
-@endforeach
+    @endforeach
     </tbody>
 </table>
 @else
