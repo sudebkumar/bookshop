3ab3e6d010246bf06905be3b0178973af9363791
sudebkumar <sudeb.phy137@gmail.com>
2014-08-25 12:42:18 +0600

    book.blade.php todoapps commit

---
+++ app/views/books/book.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+    <title>George's BookShop::Peru</title>
+    <meta charset="utf-8">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1
+                /css/bootstrap-combined.min.css" rel="stylesheet">
+   <style>
+        table form { margin-bottom: 0; }
+        form ul { margin-left: 0; list-style: none; }
+        .error { color: red; font-style: italic; }
+        body { padding-top: 20px; }
+    </style>
+</head>
+<body>
+<div class="container">
+    @if (Session::has('message'))
+    <div class="flash alert">
+        <p>{{ Session::get('message') }}</p>
+    </div>
+    @endif
+
+    @yield('main')
+</div>
+</body>
+</html>
